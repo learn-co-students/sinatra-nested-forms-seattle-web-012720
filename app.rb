804df73f4419a60afafab4e357d846026552de84
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-02-20 11:34:03 -0800

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,31 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+
+
+
+    get '/' do
+
+        erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @lol = Pirate.new(params["pirate"]["name"],params["pirate"]["weight"],params["pirate"]["height"])
+      @you_bot = params["pirate"]["name"]
+      @you_sick = params["pirate"]["weight"]
+      @you_chill = params["pirate"]["height"]
+      @sip = params["pirate"]["ships"].select{|object|object["name"]&& object["type"] && object["booty"]}
+      erb :'pirates/show'
+      #binding.pry
+    end
+    
+
+
     # code other routes/actions here
 
   end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+
+
+    attr_accessor :name, :type, :booty
+    @@all=[]
+
+    def initialize(name, type, booty)
+        @name = name
+        @weight = weight
+        @height = height
+        @@all<<self
+    end
+    def self.all
+        @@all
+
+    end
+
+    def self.clear
+       @@all.clear
+
+    end
+
+
 end
